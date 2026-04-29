9b0fc6484974e5137cf1f84f6c0d4cd91b363325
EnriqueNB <e.bejarano@parityqc.com>
2026-04-29 16:53:31 +0200

    Add mul instruction for testing.

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -7887,13 +7887,17 @@ SDValue RISCVTargetLowering::LowerOperation(SDValue Op,
   case ISD::INTRINSIC_WO_CHAIN: {
     unsigned IntNo = cast<ConstantSDNode>(Op.getOperand(0))->getZExtValue();
     
-    if (IntNo == Intrinsic::riscv_svq_add) {
+    switch (IntNo) {
+      case Intrinsic::riscv_svq_add: {
       SDValue Op1 = Op.getOperand(1);
       SDValue Op2 = Op.getOperand(2);
 
       return DAG.getNode(llvm::RISCVSVQ::SVQ_ADD, SDLoc(Op), MVT::i32, Op1, Op2);
     }
+      default:
+        return SDValue();
   }
+}
   case ISD::INTRINSIC_W_CHAIN:
     return LowerINTRINSIC_W_CHAIN(Op, DAG);
   case ISD::INTRINSIC_VOID:
